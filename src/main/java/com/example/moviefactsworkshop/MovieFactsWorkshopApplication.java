5ab7836f49ed273c05755cb4dec1f3b3e31276ac
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2022-02-21 12:21:22 +0100

    Initialization

---
+++ src/main/java/com/example/moviefactsworkshop/MovieFactsWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviefactsworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFactsWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFactsWorkshopApplication.class, args);
+    }
+
+}
